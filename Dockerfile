17c5f9b465dc7391d33221ae9d1d3a4fed670149
AngelosZa <aggeloszaimiis@gmail.com>
2025-02-26 22:36:46 +0100

    feat: docker configs

--- Dockerfile
+++ Dockerfile
@@ -17,9 +17,9 @@ RUN curl -L "https://repo1.maven.org/maven2/io/fabric8/run-java-sh/${RUN_JAVA_VE
 
 ENV JAVA_OPTIONS="-XX:-OmitStackTraceInFastThrow"
 
-COPY build/libs/*.jar /app.jar
+COPY build/libs/middlleware-inventory-0.0.1-SNAPSHOT.jar /app.jar
 
-EXPOSE 8081
+EXPOSE 8082
 
 # Use the downloaded run-java-sh script as the entry point.
-ENTRYPOINT ["/app/run-java-sh"]
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/middlleware_inventory/middlleware_inventory/configuration/springdoc/SpringDocTags.java
+++ src/main/java/com/middlleware_inventory/middlleware_inventory/configuration/springdoc/SpringDocTags.java
@@ -5,12 +5,12 @@ import io.swagger.v3.oas.models.tags.Tag;
 import java.util.List;
 
 public class SpringDocTags {
-    public static final String ORDERS = "API for orders";
-    public static final String ORDERS_DESCRIPTION = "API to manage orders";
+    public static final String INVENTORY = "API for Inventory";
+    public static final String INVENTORY_DESCRIPTION = "API to manage orders";
 
     public static List<Tag> tags() {
         return List.of(
-                new Tag().name(ORDERS).description(ORDERS_DESCRIPTION)
+                new Tag().name(INVENTORY).description(INVENTORY_DESCRIPTION)
         );
     }
 }
